f7610247c5b6707d6e8f7a1ace56b05a0071c09c
michal0x17 <michal0x17@gmail.com>
2019-09-07 11:56:12 +0200

    configure and start Endpoint

--- practical-guide/1-configure-endpoint/src/ClientUI/Program.fs
+++ practical-guide/1-configure-endpoint/src/ClientUI/Program.fs
@@ -1,8 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open NServiceBus
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    
+    async {
+ 
+        let endpointName = "Client"
+        Console.Title <- endpointName
+
+        let endpointConfiguration = new EndpointConfiguration(endpointName)
+        
+        let transport = endpointConfiguration.UseTransport<LearningTransport>()
+
+        let! endpointInstance = Endpoint.Start(endpointConfiguration) |> Async.AwaitTask
+            
+        printf "Press Enter to exit..."
+        Console.ReadLine() |> ignore
+
+        do! endpointInstance.Stop() |> Async.AwaitTask
+
+    } |> Async.RunSynchronously
+    
     0 // return an integer exit code
