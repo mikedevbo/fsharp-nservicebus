9c05b6e670ad1a0d985d0b0f30eb0b33c74680d6
mikedevbo <mike@devbo.pl>
2020-11-07 05:57:04 +0100

    user fsharp 5 feature
    'Allow implementing the same interface at different generic instantiations'
    
    for Saga implementation

--- practical-guide/5-configure-and-use-saga/src/Shipping/ShippingPolicy.fs
+++ practical-guide/5-configure-and-use-saga/src/Shipping/ShippingPolicy.fs
@@ -12,51 +12,32 @@ type ShippingPolicyData() =
     member val IsOrderPlaced = false with get,set
     member val IsOrderBilled = false with get,set
 
-let canShipOrder (policyData: ShippingPolicyData) =
-    policyData.IsOrderPlaced && policyData.IsOrderBilled
-
-let shipOrder orderId (context: IMessageHandlerContext) =
-    let shipOrder = new ShipOrder(orderId)
-    context.SendLocal(shipOrder)
-
-type ShippingPolicyOrderPlaced() =
+type ShippingPolicy() =
     inherit Saga<ShippingPolicyData>()
         override this.ConfigureHowToFindSaga(mapper: SagaPropertyMapper<ShippingPolicyData>) =
             mapper.ConfigureMapping<OrderPlaced>(fun message -> message.OrderId :> obj).ToSaga(fun sagaData -> sagaData.OrderId :> obj)
-    
-    static member log = LogManager.GetLogger<ShippingPolicyOrderPlaced>()
-    
-    interface IAmStartedByMessages<OrderPlaced> with
-        member this.Handle(message, context) = 
-            ShippingPolicyOrderPlaced.log.Info("OrderPlaced message received.")
-            
-            this.Data.IsOrderPlaced <- true
-            
-            let canShipOrder = canShipOrder this.Data
-            match canShipOrder with
-            | true ->
-                this.MarkAsComplete()
-                shipOrder this.Data.OrderId context
-            | false ->
-                Task.CompletedTask
-
-type ShippingPolicyOrderBilled() =
-    inherit Saga<ShippingPolicyData>()
-        override this.ConfigureHowToFindSaga(mapper: SagaPropertyMapper<ShippingPolicyData>) =
             mapper.ConfigureMapping<OrderBilled>(fun message -> message.OrderId :> obj).ToSaga(fun sagaData -> sagaData.OrderId :> obj)
-            
-    static member log = LogManager.GetLogger<ShippingPolicyOrderBilled>()
-    
+    static member log = LogManager.GetLogger<ShippingPolicy>()
+
+    interface IAmStartedByMessages<OrderPlaced> with
+        member this.Handle(message, context) =
+            ShippingPolicy.log.Info("OrderPlaced message received.")
+
+            this.Data.IsOrderPlaced <- true
+            this.ProcessOrder(context)
+
     interface IAmStartedByMessages<OrderBilled> with
-        member this.Handle(message, context) = 
-            ShippingPolicyOrderBilled.log.Info("OrderBilled message received.")
-            
+        member this.Handle(message, context) =
+            ShippingPolicy.log.Info("OrderBilled message received.")
+
             this.Data.IsOrderBilled <- true
-            
-            let canShipOrder = canShipOrder this.Data
-            match canShipOrder with
-            | true ->
-                this.MarkAsComplete()
-                shipOrder this.Data.OrderId context
-            | false ->
-                Task.CompletedTask
+            this.ProcessOrder(context)
+
+    member this.ProcessOrder(context:IMessageHandlerContext) =
+        match this.Data.IsOrderPlaced && this.Data.IsOrderBilled with
+        | true ->
+            this.MarkAsComplete()
+            let shipOrder = new ShipOrder(this.Data.OrderId)
+            context.SendLocal(shipOrder)
+        | false ->
+            Task.CompletedTask
